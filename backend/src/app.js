9d340c914e93bd959daf83ae37601b0ffaacbf57
KAYO <yohanolivier.koffi@gmail.com>
2026-06-21 23:11:28 +0000

    modification du port

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,8 @@
 // Backend application entry point
 import { app } from "./routes/routes.js"
 
-app.listen(3000,() => {
-    console.log("✅ Server running on http://192.168.1.29:3000")
-    console.log("✅ CORS enabled for all origins")
+const PORT = process.env.PORT || 3000
+app.listen(PORT,() => {
+    console.log(`✅ Server running on port ${PORT}`)
 })
 
