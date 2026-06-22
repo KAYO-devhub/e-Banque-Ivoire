ee716274097cdc3beb2bcdf2eca0da35d94514b7
KAYO <yohanolivier.koffi@gmail.com>
2026-06-22 11:01:11 +0000

    ajout du certificat ssl demander par aiven

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -5,5 +5,7 @@ export const pool = mysql2.createPool({
     host:process.env.MYSQL_HOST,
     user:process.env.MYSQL_USER,
     password:process.env.MYSQL_PASSWORD,
-    database:process.env.MYSQL_DATABASE
+    database:process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT,
+    ssl: {rejectUnauthorized: false}
 }).promise()
