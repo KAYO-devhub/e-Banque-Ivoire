b0854a2a7adeb1762444c05f5ee926a57addeee1
KAYO <yohanolivier.koffi@gmail.com>
2026-06-26 17:33:39 +0000

    test de l'implémentation des noms et prénoms

--- backend/src/services/upload.js
+++ backend/src/services/upload.js
@@ -31,8 +31,8 @@ export const storage = multer.diskStorage({
         // 2. Récupération et nettoyage du Nom / Prénom (Via ton middleware JWT req.user)
         const nom = await pool.query(`SELECT nom FROM users WHERE uuid = ?`,[req.body.user_uuid]) || 'nom';
         const prenom = await pool.query(`SELECT prenom FROM users WHERE uuid = ?`,[req.body.user_uuid]) || 'prenom';
-        const nomPropre = nom[0]
-        const prenomPropre = prenom[0]
+        const nomPropre = nom
+        const prenomPropre = prenom
         
         // 3. Extraction de l'extension (.pdf, .jpg) en minuscules
         const extension = path.extname(file.originalname).toLowerCase();
@@ -47,6 +47,7 @@ export const storage = multer.diskStorage({
         
         cb(null, nomFichierFinal); 
     }
+    
 });
 
 export const upload = multer({ storage: storage });
